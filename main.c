34d1ff5f5982a4a08e2b3884227b207b4b1459a1
Askajafar <58959241+Askajafar@users.noreply.github.com>
2023-05-10 23:55:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,33 @@
 
 #include "avl.h"
 
-struct nodo *destroiNodo(struct nodo *nodo){
+void rotacaoDireita(struct nodo **raiz){
+    struct nodo *p = *raiz;
+    struct nodo *q = p->fe;
+    if(q->balance >= 0)
+        p->balance++;
+    else
+        p->balance = p->balance + 2;
+    q->balance++;
+    if(p->pai == NULL){
+        q->pai = NULL;
+    }
+    else{
+        if(p == p->pai->fe)
+            p->pai->fe = q;
+        else
+            p->pai->fd = q;
+        q->pai = p->pai;
+    }
+    p->pai = q;
+    p->fe = q->fd;
+    if(q->fd != NULL)
+        q->fd->pai = p;
+    q->fd = p;
+    *raiz = q;
+}
+
+struct nodo *destruirNodo(struct nodo *nodo){
     free(nodo);
     nodo = NULL;
     return nodo;
@@ -15,20 +41,29 @@ void destroiArvore(struct nodo* nodo){
 
 	destroiArvore(nodo->fe);
 	destroiArvore(nodo->fd);
-	destroiNodo(nodo);
+	destruirNodo(nodo);
 	return;
 }
 
 int main(){
     struct nodo *raiz = NULL;
-	imprimirDadosAlunos();
-    inserir(&raiz, 1);
-	inserir(&raiz, 5);
-	inserir(&raiz, 3);
-	inserir(&raiz, 2);
-	inserir(&raiz, 4);
+    
+    imprimirDadosAlunos();
 
-	imprimirEmOrdem(raiz);
+    inserir(&raiz, 4);
+    inserir(&raiz, 2);
+    //inserir(&raiz, 7);
+    inserir(&raiz, 1);
+    //inserir(&raiz, 3);
+    //inserir(&raiz, 6);
+    //inserir(&raiz, 8);
+
+    imprimirEmLargura(raiz);
+    printf("\n");
+    rotacaoDireita(&raiz);
+    imprimirEmLargura(raiz);
+
+   
     destroiArvore(raiz);
 	return 0;
 }
