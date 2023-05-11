beed90aab615816e8b82c4c7a0dc9efedef40ce0
Askajafar <58959241+Askajafar@users.noreply.github.com>
2023-05-10 23:53:52 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -46,12 +46,11 @@ struct fila* destroi_fila (struct fila *f){
     return NULL;
 }
 
-
 /*
  * Insere o elemento no final da fila (politica FIFO).
  * Retorna 1 se a operacao foi bem sucedida e 0 caso contrario.
  */
-int insere_fila (struct fila *f, struct nodo* nodo){
+int insere_fila (struct fila **f, struct nodo* nodo){
     struct nodo_fila *novo;
 
     novo = malloc(sizeof(struct nodo_fila));
@@ -60,15 +59,14 @@ int insere_fila (struct fila *f, struct nodo* nodo){
     novo->nodo = nodo;
     novo->prox = NULL;
 
-    if (vazia_fila(f)){
-        f->inicio = novo;
-        f->fim = novo;
-
+    if (vazia_fila(*f)){
+        (*f)->inicio = novo;
+        (*f)->fim = novo;
         return 1;
     }
 
-    f->fim->prox = novo;
-    f->fim = novo;
+    (*f)->fim->prox = novo;
+    (*f)->fim = novo;
     return 1;
 
 }
@@ -77,17 +75,16 @@ int insere_fila (struct fila *f, struct nodo* nodo){
  * Remove o elemento do iniciocio da fila e o retorna.
  * Retorna 1 se a operacao foi bem sucedida e 0 caso contrario.
  */
-struct nodo* retira_fila (struct fila *f){
+struct nodo* retira_fila (struct fila **f){
     struct nodo_fila* pont;
     struct nodo* nodo;
 
-    if (vazia_fila(f))
+    if (vazia_fila(*f))
         return 0;
 
-    nodo = f->inicio->nodo;
-    pont = f->inicio->prox;
-    free(f->inicio);
-    f->inicio = pont;
-
+    nodo = (*f)->inicio->nodo;
+    pont = (*f)->inicio->prox;
+    free((*f)->inicio);
+    (*f)->inicio = pont;
     return nodo;
 }
