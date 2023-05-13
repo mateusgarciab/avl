9d1a59eaf65bedeca556137811ad9abed22a83f5
Askajafar <58959241+Askajafar@users.noreply.github.com>
2023-05-12 22:56:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -20,28 +20,39 @@ void destroiArvore(struct nodo* nodo){
 }
 
 int main(){
-    struct nodo *raiz = NULL;
-    
+    struct nodo *raiz = NULL, *retorno2;
+    char op;
+    int chave, retorno;
     imprimirDadosAlunos();
-
-    //inserir(&raiz, 4);
-    //inserir(&raiz, 2);
-    //inserir(&raiz, 7);
-
-    inserir(&raiz, 3);
-
-    inserir(&raiz, 1);
-           imprimirEmLargura(raiz);
-    inserir(&raiz, 2);
- 
-    //inserir(&raiz, 8);
-
-
-    printf("\n");
-
-    imprimirEmLargura(raiz);
-
-   
+    scanf("%c %d", &op, &chave);
+    while(op != 'f'){
+        switch(op){
+            case 'i':
+                retorno2 = inserir(&raiz, chave);
+                if(retorno2 == NULL)
+                    printf("Falha ao inserir.\n");
+            break;
+            case 'r':
+                retorno = excluir(&raiz, chave);
+                if(retorno == 0)
+                    printf("Falha ao remover %d.\n", chave);
+            break;
+            case 'b':
+                retorno2 = buscar(raiz, chave);
+                if(retorno2 == NULL)
+                    printf("Nao encontrado %d.\n", chave);
+                else
+                    printf("Encontrado %d.\n", retorno2->chave);
+            break;
+            case 'e':
+                imprimirEmOrdem(raiz);
+            break;
+            case 'l':
+                imprimirEmLargura(raiz);
+            break;
+        }
+        scanf("%c %d", &op, &chave);
+    }
     destroiArvore(raiz);
 	return 0;
 }
